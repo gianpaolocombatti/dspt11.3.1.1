e0989d781b46aa2c97342df37a9ac03e39dd3865
John Hurdle <john.renken@lambdaschool.com>
2021-05-12 20:16:37 -0700

    end of sprint 1 module 4

--- mymodule/reddit.py
+++ mymodule/reddit.py
@@ -8,13 +8,29 @@ class Post:
 
 class User:
     def __init__(self, name, reputation, is_moderator):
+        """
+        Create a User object
+        ----------
+        title : str
+        Returns
+        -------
+        Topic
+        """
         self.reputation = reputation
         self.name = name
         self.is_moderator = is_moderator
         self.topics = []
         self.comments = []
 
-    def post_topic(self, title, body):
+    def post_topic(self, title="", body=""):
+        """
+        Post a topic with the user as the author and the given title and body
+        ----------
+        title : str
+        Returns
+        -------
+        Topic
+        """
         topic = Topic(title=title, body=body, user=self)
         self.topics.append(topic)
         return topic
@@ -31,13 +47,16 @@ class User:
 class Topic(Post):
     def __init__(self, title, user, body, up_votes=0, down_votes=0):
         self.title = title
-        super(Topic, self).__init__(up_votes=up_votes, down_votes=down_votes, user=user, body=body)
+        self.comments = []
+        self.comments.append(Comment(body='This is my topic', user=user, topic=self))
+        super(Topic, self).__init__(
+            up_votes=up_votes, down_votes=down_votes, user=user, body=body
+        )
 
 
 class Comment(Post):
     def __init__(self, topic, body, user, up_votes=0, down_votes=0):
         self.topic = topic
-        super(Comment, self).__init__(up_votes=up_votes, down_votes=down_votes, user=user, body=body)
-
-
-
+        super(Comment, self).__init__(
+            up_votes=up_votes, down_votes=down_votes, user=user, body=body
+        )
